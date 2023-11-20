9473e6bdd19096a555a2f88a97a6f9fc2f06a9af
Md Nazrul Islam <arfanislamabir@gmail.com>
2023-11-21 01:29:22 +0600

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("welcome to my custom logger")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>>>>>>>>>>>>>> stage {STAGE_NAME} Started <<<<<<<<<<<<<<<<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>>>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<<<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e 
